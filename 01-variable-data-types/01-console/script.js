e97157cb5a5b43304e47323a62312ad3d28291d8
Artyom Gerasimov <tourmalineschoerl@gmail.com>
2025-05-01 01:24:52 +0300

    Initial commit

---
+++ 01-variable-data-types/01-console/script.js
@@ -0,0 +1,25 @@
+console.log(100);
+
+console.log('Hello World');
+
+console.log(11, 'Bye', true);
+
+const x = 100;
+
+console.log(x);
+
+console.warn('Warning');
+
+console.error('Error');
+
+console.table({ name: 'John', email: 'john@gmail.com' });
+
+console.group('simple');
+console.log(x);
+console.warn('Warning');
+console.error('Error');
+console.groupEnd();
+
+const styles = 'padding: 10px; background-color: white; color: green';
+
+console.log('%cHello World', styles);
