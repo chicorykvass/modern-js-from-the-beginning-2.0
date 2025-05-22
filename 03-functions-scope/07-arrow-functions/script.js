8296c7e87c3a2e3524a346d92a3ba000dd7d351c
Artyom Gerasimov <tourmalineschoerl@gmail.com>
2025-05-22 12:41:38 +0300

    Fix trailing slashes on void elements
    Add Prettier config with plugin to correct this behavior

---
+++ 03-functions-scope/07-arrow-functions/script.js
@@ -0,0 +1,17 @@
+// function add(a, b) {
+//   return a + b;
+// }
+
+// Arrow function syntax
+const add = (a, b) => {
+  return a + b;
+};
+
+// Implicit return
+const subtract = (a, b) => a - b;
+
+// const double = a => a * 2;
+
+console.log(add(1, 2));
+console.log(subtract(8, 1));
+// console.log(double(6));
