78abc5c99382f766c35be4f93fb138a5dcd10836
Artyom Gerasimov <tourmalineschoerl@gmail.com>
2025-07-23 14:12:10 +0300

    Studying

---
+++ 05-iteration-array-methods/06-for-in-loop/script.js
@@ -0,0 +1,16 @@
+const colorObj = {
+  color1: 'red',
+  color2: 'yellow',
+  color3: 'green',
+  color4: 'blue',
+};
+
+// for (const key in colorObj) {
+//   console.log(key, colorObj[key]);
+// }
+
+const colorArr = ['red', 'yellow', 'green', 'blue'];
+
+for (const idx in colorArr) {
+  console.log(idx, colorArr[idx]);
+}
