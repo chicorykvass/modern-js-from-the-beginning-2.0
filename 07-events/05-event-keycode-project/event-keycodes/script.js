f4444583bb422f76e4463a6732e0986742deb994
Artyom Gerasimov <tourmalineschoerl@gmail.com>
2025-08-01 22:22:46 +0300

    Optimize code: remove array, replace array element with a single variable

--- 07-events/05-event-keycode-project/event-keycodes/script.js
+++ 07-events/05-event-keycode-project/event-keycodes/script.js
@@ -2,23 +2,21 @@ function showKeyInfo(e) {
   const insert = document.querySelector('#insert');
   insert.innerHTML = '';
 
-  const divs = [];
-
   for (let i = 0; i < 3; i++) {
-    divs[i] = document.createElement('div');
-    divs[i].className = 'key';
+    const div = document.createElement('div');
+    div.className = 'key';
 
     const small = document.createElement('small');
 
-    [divs[i].textContent, small.textContent] =
+    [div.textContent, small.textContent] =
       i === 0
         ? [e.key === ' ' ? 'Space' : e.key, 'e.key']
         : i === 1
           ? [e.keyCode, 'e.keyCode']
           : [e.code, 'e.code'];
 
-    divs[i].appendChild(small);
-    insert.appendChild(divs[i]);
+    div.appendChild(small);
+    insert.appendChild(div);
   }
 }
 
